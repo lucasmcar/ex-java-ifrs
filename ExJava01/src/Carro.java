afcf5498043ddead25de5aab02ee218f4e6f28e9
lucasmcar <lmc.lucas90@gmail.com>
2021-10-23 12:27:07 -0300

    Exercicios 1 e 2 de Java do IFRS

---
+++ ExJava01/src/Carro.java
@@ -0,0 +1,39 @@
+public class Carro {
+
+    private String tipo;
+    private String cor;
+    private String placa;
+    private int numPortas;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public int getNumPortas() {
+        return numPortas;
+    }
+
+    public void setNumPortas(int numPortas) {
+        this.numPortas = numPortas;
+    }
+}
